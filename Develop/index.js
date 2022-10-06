ec0ddf9b77875be7870d54b983ac37736b934e61
Ivan C <cordovaivan8@gmail.com>
2022-10-05 18:01:34 -0700

    added index.js and gitignore file

---
+++ Develop/index.js
@@ -0,0 +1,4 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const generateREADME = ({Title, Description, TableofContents, Installation, Usage, License, Contributing, Tests, Github})
